6cfb08bd1297dd127e2c3574dab0e83970d25562
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-02-28 16:52:29 +0000

    clean up plotting

--- calculate_formation_energies.py
+++ calculate_formation_energies.py
@@ -29,6 +29,56 @@ def read_ey(file):
     return ey*2625.5
 
 
+def plot_FE(
+    X,
+    Y,
+    leg_info,
+    names,
+    same_sizers,
+    ylbl,
+    X_pos,
+    title,
+    amine=None
+):
+
+    fig, ax = plt.subplots(figsize=(8, 5))
+    for ami in X:
+        if amine is not None:
+            if amine != ami:
+                continue
+        lab = leg_info[ami]['label']
+        c = leg_info[ami]['c']
+        for i in range(len(X[ami])):
+            X_value = X[ami][i]
+            Y_value = Y[ami][i] - min(Y[ami])
+            print(X_value, Y_value)
+            if i == 0:
+                flat_line(
+                    ax, x=X_value, y=Y_value,
+                    w=1, C=c, label=lab
+                )
+            else:
+                flat_line(ax, x=X_value, y=Y_value, w=1, C=c)
+            if names[ami][i] in same_sizers:
+                ax.text(
+                    x=X_value-4,
+                    y=Y_value-5,
+                    s=names[ami][i]
+                )
+
+    ax.legend(fontsize=16)
+    # ax.axhline(y=0, c='k', alpha=0.2, lw=2)
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.set_xlabel('cluster size', fontsize=16)
+    ax.set_xlim(0, 30)
+    # ax.set_ylim(-150, 150)
+    ax.set_ylabel(ylbl, fontsize=16)
+    ax.set_xticks(list(X_pos.values()))
+    ax.set_xticklabels(list(X_pos.keys()))
+    fig.tight_layout()
+    fig.savefig(title, dpi=720, bbox_inches='tight')
+
+
 def main():
     X_positions = {
         '[1+1]': 3,
@@ -97,79 +147,52 @@ def main():
         Y_values_pI[rxn['ami']].append(FE_pI)
         names[rxn['ami']].append(rxn['long-name'])
 
-    fig, ax = plt.subplots(figsize=(8, 5))
-    for ami in X_values:
-        lab = leg_info[ami]['label']
-        c = leg_info[ami]['c']
-        for i in range(len(X_values[ami])):
-            X_value = X_values[ami][i]
-            Y_value = Y_values[ami][i] - min(Y_values[ami])
-            print(X_value, Y_value)
-            if i == 0:
-                flat_line(
-                    ax, x=X_value, y=Y_value,
-                    w=1, C=c, label=lab
-                )
-            else:
-                flat_line(ax, x=X_value, y=Y_value, w=1, C=c)
-            if names[ami][i] in same_sizers:
-                ax.text(
-                    x=X_value-4,
-                    y=Y_value-5,
-                    s=names[ami][i]
-                )
-
-    ax.legend(fontsize=16)
-    # ax.axhline(y=0, c='k', alpha=0.2, lw=2)
-    ax.tick_params(axis='both', which='major', labelsize=16)
-    ax.set_xlabel('cluster size', fontsize=16)
-    ax.set_xlim(0, 30)
-    # ax.set_ylim(-150, 150)
-    ax.set_ylabel(
-        'free energy of formation [kJ/mol]',
-        fontsize=16
+    plot_FE(
+        X=X_values,
+        Y=Y_values,
+        leg_info=leg_info,
+        names=names,
+        same_sizers=same_sizers,
+        ylbl='free energy of formation [kJ/mol]',
+        X_pos=X_positions,
+        title='fe_total.pdf',
+        amine=None
     )
-    ax.set_xticks(list(X_positions.values()))
-    ax.set_xticklabels(list(X_positions.keys()))
-    fig.tight_layout()
-    fig.savefig('fe_total.pdf', dpi=720, bbox_inches='tight')
-
-    fig, ax = plt.subplots(figsize=(8, 5))
-    for ami in X_values:
-        lab = leg_info[ami]['label']
-        c = leg_info[ami]['c']
-        for i in range(len(X_values[ami])):
-            X_value = X_values[ami][i]
-            Y_value = Y_values_pI[ami][i] - min(Y_values_pI[ami])
-            print(X_value, Y_value)
-            if i == 0:
-                flat_line(
-                    ax, x=X_value, y=Y_value,
-                    w=1, C=c, label=lab
-                )
-            else:
-                flat_line(ax, x=X_value, y=Y_value, w=1, C=c)
-            if names[ami][i] in same_sizers:
-                ax.text(
-                    x=X_value-4,
-                    y=Y_value-3,
-                    s=names[ami][i]
-                )
-
-    ax.legend(fontsize=16)
-    # ax.axhline(y=0, c='k', alpha=0.2, lw=2)
-    ax.tick_params(axis='both', which='major', labelsize=16)
-    ax.set_xlabel('cluster size', fontsize=16)
-    ax.set_xlim(0, 30)
-    # ax.set_ylim(-150, 150)
-    ax.set_ylabel(
-        'free energy of formation\nper imine bond [kJ/mol]',
-        fontsize=16
+    plot_FE(
+        X=X_values,
+        Y=Y_values_pI,
+        leg_info=leg_info,
+        names=names,
+        same_sizers=same_sizers,
+        ylbl='free energy of formation\nper imine bond [kJ/mol]',
+        X_pos=X_positions,
+        title='fe_total.pdf',
+        amine=None
     )
-    ax.set_xticks(list(X_positions.values()))
-    ax.set_xticklabels(list(X_positions.keys()))
-    fig.tight_layout()
-    fig.savefig('fe_perimine.pdf', dpi=720, bbox_inches='tight')
+
+    for ami in X_values:
+        plot_FE(
+            X=X_values,
+            Y=Y_values,
+            leg_info=leg_info,
+            names=names,
+            same_sizers=same_sizers,
+            ylbl='free energy of formation [kJ/mol]',
+            X_pos=X_positions,
+            title=f'fe_total_{ami}.pdf',
+            amine=ami
+        )
+        plot_FE(
+            X=X_values,
+            Y=Y_values_pI,
+            leg_info=leg_info,
+            names=names,
+            same_sizers=same_sizers,
+            ylbl='free energy of formation\nper imine bond [kJ/mol]',
+            X_pos=X_positions,
+            title=f'fe_perimine_{ami}.pdf',
+            amine=ami
+        )
 
 
 if __name__ == '__main__':
