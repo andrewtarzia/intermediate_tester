6a80d5a23fc133ea1041a873e53b2fa6492e8303
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-03-30 17:34:16 +0100

    Make work on new stk.

--- build_cages.py
+++ build_cages.py
@@ -15,29 +15,43 @@ import stk
 
 
 def main():
-    alde = stk.BuildingBlock.init_from_file('alde1.mol', ['aldehyde'])
-    ami1 = stk.BuildingBlock.init_from_file('ami1.mol', ['amine'])
+    alde = stk.BuildingBlock.init_from_file(
+        'alde1.mol', [stk.AldehydeFactory()]
+    )
+    ami1 = stk.BuildingBlock.init_from_file(
+        'ami1.mol', [stk.PrimaryAminoFactory()]
+    )
     cage1 = stk.ConstructedMolecule(
-        building_blocks=[alde, ami1],
-        topology_graph=stk.cage.FourPlusSix()
+        topology_graph=stk.cage.FourPlusSix(
+            building_blocks=(alde, ami1),
+        )
     )
     cage1.write('4_alde1_6_ami1_1.mol')
-    ami2 = stk.BuildingBlock.init_from_file('ami2.mol', ['amine'])
+    ami2 = stk.BuildingBlock.init_from_file(
+        'ami2.mol', [stk.PrimaryAminoFactory()]
+    )
     cage2 = stk.ConstructedMolecule(
-        building_blocks=[alde, ami2],
-        topology_graph=stk.cage.FourPlusSix()
+        topology_graph=stk.cage.FourPlusSix(
+            building_blocks=(alde, ami2),
+        )
     )
     cage2.write('4_alde1_6_ami2_1.mol')
-    ami3 = stk.BuildingBlock.init_from_file('ami3.mol', ['amine'])
+    ami3 = stk.BuildingBlock.init_from_file(
+        'ami3.mol', [stk.PrimaryAminoFactory()]
+    )
     cage3 = stk.ConstructedMolecule(
-        building_blocks=[alde, ami3],
-        topology_graph=stk.cage.FourPlusSix()
+        topology_graph=stk.cage.FourPlusSix(
+            building_blocks=(alde, ami3),
+        )
     )
     cage3.write('4_alde1_6_ami3_1.mol')
-    ami4 = stk.BuildingBlock.init_from_file('ami4.mol', ['amine'])
+    ami4 = stk.BuildingBlock.init_from_file(
+        'ami4.mol', [stk.PrimaryAminoFactory()]
+    )
     cage4 = stk.ConstructedMolecule(
-        building_blocks=[alde, ami4],
-        topology_graph=stk.cage.FourPlusSix()
+        topology_graph=stk.cage.FourPlusSix(
+            building_blocks=(alde, ami4),
+        )
     )
     cage4.write('4_alde1_6_ami4_1.mol')
 
