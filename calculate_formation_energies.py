2023a83e434d5df9e36902b00a7fae3f2001ec43
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-12-10 17:17:38 +0000

    Bring into one script and clean up.

--- calculate_formation_energies.py
+++ calculate_formation_energies.py
@@ -17,82 +17,114 @@ import pandas as pd
 from reactions import landscape
 
 
+def get_molecule_energy_from_frame(frame, molecule_name, method):
+    by_name = frame[frame['name'] == molecule_name]
+    try:
+        by_method = float(by_name[method].iloc[0])
+    except ValueError:
+        raise ValueError(f'Issue with {molecule_name} and {method}')
+    return by_method
+
+
 def main():
     if len(sys.argv) != 4:
         print(
-            'csv file with DFT energies and output csv is required'
-            ' - solv is also req: gas or chcl3.'
+            'csv file with all DFT energies and output csv is required'
         )
     else:
         ey_file = sys.argv[1]
-        output_name = sys.argv[2]
-        solv = sys.argv[3]
+        formation_energy_file = sys.argv[2]
+        per_imine_file = sys.argv[3]
 
-    print(ey_file, output_name)
-    extracted_ey = pd.read_csv(ey_file)
-    extracted_ey['clean_name'] = [i for i in extracted_ey['names']]
-    energy_dict = {
-        i: j
-        for i, j in zip(
-            extracted_ey['clean_name'],
-            extracted_ey[f'energy_{solv}'],
-        )
-    }
-    print(energy_dict)
+    all_structure_energies = pd.read_csv(ey_file)
+    print(all_structure_energies)
+
+    _energy_methods = set(
+        list(all_structure_energies.columns)
+    )
+    print(_energy_methods)
+    # Get the landscape of intermediates.
     lscp = landscape()
-    df = pd.DataFrame({
-        'intname': [i['intname'] for i in lscp],
-        'long-name': [i['long-name'] for i in lscp],
-    })
 
-    energies = []
-    prod_energies = []
-    react_energies = []
-    formation_energies = []
-    formation_energies_per_imine = []
-    for intermediate in lscp:
-        iname = intermediate['intname']
-        if intermediate['intname'] in energy_dict:
-            # Get energies.
-            ey = energy_dict[iname]
-            prodey = sum(
-                [
-                    energy_dict[i.replace('_opt_xtb', '')]
-                    for i in intermediate['prod']
-                ]
-            )
-            reactey = sum(
-                [
-                    energy_dict[i.replace('_opt_xtb', '')]
-                    for i in intermediate['react']
-                ]
-            )
-            formey = prodey - reactey
+    all_formation_energies = {}
+    all_formation_energies_per_imine = {}
+    for method in _energy_methods:
+        all_formation_energies[method] = {}
+        all_formation_energies_per_imine[method] = {}
+        for intermediate in lscp:
+            iname = intermediate['intname']
+
+            # Sum product energies.
+            prodey = [
+                get_molecule_energy_from_frame(
+                    frame=all_structure_energies,
+                    molecule_name=i,
+                    method=method,
+                )
+                for i in intermediate['prod']
+            ]
+
+            # Sum reactant energies.
+            reactey = [
+                get_molecule_energy_from_frame(
+                    frame=all_structure_energies,
+                    molecule_name=i,
+                    method=method,
+                )
+                for i in intermediate['react']
+            ]
+
+            formey = sum(prodey) - sum(reactey)
             formeyperimine = formey / intermediate['no.imine']
             if formey > 0:
-                input('he')
+                prods = intermediate['prod']
+                react = intermediate['react']
+                print(
+                    f'formation energy ({formey}) is positive for '
+                    f'{iname} using {method}.\n'
+                    f'products: {prods} with ey: {prodey}\n'
+                    f'ractants: {react} with ey: {reactey}\n\n'
+                )
+            all_formation_energies[method][iname] = formey
+            all_formation_energies_per_imine[method][iname] = (
+                formeyperimine
+            )
 
-            energies.append(ey)
-            prod_energies.append(prodey)
-            react_energies.append(reactey)
-            formation_energies.append(formey)
-            formation_energies_per_imine.append(formeyperimine)
-        else:
-            raise ValueError()
+    with open(formation_energy_file, 'w') as f:
+        # Write top line.
+        top_line = 'name'
+        for method in all_formation_energies:
+            top_line += f',{method}'
+        top_line += '\n'
+        f.write(top_line)
+        # Rows.
+        for intermediate in lscp:
+            iname = intermediate['intname']
+            line = iname
+            for method in all_formation_energies:
+                energy = all_formation_energies[method][iname]
+                line += f',{energy}'
+            line += '\n'
+            f.write(line)
 
-    df['energy_au'] = energies
-    df['prodenergy_au'] = prod_energies
-    df['reactenergy_au'] = react_energies
-    df['formenergy_au'] = formation_energies
-    df['formenergypermine_au'] = formation_energies_per_imine
-    df['formenergy_kjmol'] = [
-        None if i is None else i*2625.5 for i in formation_energies
-    ]
-    df['formenergypermine_kjmol'] = [
-        None if i is None else i*2625.5 for i in formation_energies_per_imine
-    ]
-    print(df)
-    df.to_csv(output_name, index=False)
+    with open(per_imine_file, 'w') as f:
+        # Write top line.
+        top_line = 'name'
+        for method in all_formation_energies:
+            top_line += f',{method}'
+        top_line += '\n'
+        f.write(top_line)
+        # Rows.
+        for intermediate in lscp:
+            iname = intermediate['intname']
+            line = iname
+            for method in all_formation_energies_per_imine:
+                energy = (
+                    all_formation_energies_per_imine[method][iname]
+                )
+                line += f',{energy}'
+            line += '\n'
+            f.write(line)
 
 
 if __name__ == '__main__':
