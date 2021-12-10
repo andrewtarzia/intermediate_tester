7847fdb7606247c1b140964d2edbab838ca5fd39
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-12-10 16:04:12 +0000

    Add formation energy calculation for dft.

--- calculate_dftspe_formation_energies.py
+++ calculate_dftspe_formation_energies.py
@@ -23,9 +23,13 @@ def clean_extracted_dictionary(extracted_energy_file):
         i.split('_opt')[0] for i in extracted_ey['name']
     ]
     extracted_ey['solvent'] = [
-        i.split('_opt_')[1] for i in extracted_ey['name']
+        i.split('_opt_')[1].split('_')[0]
+        for i in extracted_ey['name']
     ]
-
+    extracted_ey['method'] = [
+        i.split('_opt_')[1].split('_')[1].replace('.gau', '')
+        for i in extracted_ey['name']
+    ]
     print(extracted_ey)
     return extracted_ey
 
@@ -44,16 +48,18 @@ def get_molecule_energy_from_frame(frame, molecule_name, key):
 def main():
     if len(sys.argv) != 3:
         print(
-            'csv file with DFT energies and output csv is required'
+            'csv file with DFT energies and prefix for output csv '
+            'is required'
         )
         sys.exit()
     else:
         ey_file = sys.argv[1]
-        output_name = sys.argv[2]
+        output_prefix = sys.argv[2]
 
+    _methods = ('pbe', 'mp2')
     _solvents = ('gas', 'dcm', 'cfm')
     _columns = (
-        'intname', 'long-name', 'solvent', 'energy_au',
+        'intname', 'long-name', 'solvent', 'method', 'energy_au',
         'prodenergy_au', 'reactenergy_au', 'formenergy_au',
         'formenergypermine_au',
     )
@@ -61,60 +67,69 @@ def main():
     energy_frame = clean_extracted_dictionary(ey_file)
 
     lscp = landscape()
-    output_df = pd.DataFrame(columns=_columns)
 
-    count = 1
-    for intermediate in lscp:
-        iname = intermediate['intname']
-        lname = intermediate['long-name']
+    for meth in _methods:
+        m_frame = energy_frame[energy_frame['method'] == meth]
         for solv in _solvents:
-            s_frame = energy_frame[energy_frame['solvent'] == solv]
-            # Get total energy.
-            ey = get_molecule_energy_from_frame(
-                frame=s_frame,
-                molecule_name=iname,
-                key='energy',
-            )
-            # Sum product energies.
-            prodey = sum(
-                [
-                    get_molecule_energy_from_frame(
-                        frame=s_frame,
-                        # Modify name from xtb code.
-                        molecule_name=i.replace('_opt_xtb', ''),
-                        key='energy',
-                    )
-                    for i in intermediate['prod']
+            s_frame = m_frame[m_frame['solvent'] == solv]
+            output_df = pd.DataFrame(columns=_columns)
+            count = 1
+            for intermediate in lscp:
+                iname = intermediate['intname']
+                lname = intermediate['long-name']
+
+                # Get total energy.
+                ey = get_molecule_energy_from_frame(
+                    frame=s_frame,
+                    molecule_name=iname,
+                    key='energy',
+                )
+                # Sum product energies.
+                prodey = sum(
+                    [
+                        get_molecule_energy_from_frame(
+                            frame=s_frame,
+                            # Modify name from xtb code.
+                            molecule_name=i.replace('_opt_xtb', ''),
+                            key='energy',
+                        )
+                        for i in intermediate['prod']
+                    ]
+                )
+                # Sum reactant energies.
+                reactey = sum(
+                    [
+                        get_molecule_energy_from_frame(
+                            frame=s_frame,
+                            # Modify name from xtb code.
+                            molecule_name=i.replace('_opt_xtb', ''),
+                            key='energy',
+                        )
+                        for i in intermediate['react']
+                    ]
+                )
+
+                # Get reactant energies.
+                formey = prodey - reactey
+                formeyperimine = formey / intermediate['no.imine']
+                if formey > 0:
+                    print(intermediate, solv, formey, formeyperimine)
+
+                # Add series to dataframe.
+                output_df.loc[count] = [
+                    iname, lname, solv, ey, prodey, reactey, formey,
+                    formeyperimine,
                 ]
+                count += 1
+
+            print(output_df)
+            output_df.to_csv(
+                (
+                    f'{output_prefix}_{solv}_'
+                    f'{meth}_formation_energies.csv'
+                ),
+                index=False,
             )
-            # Sum reactant energies.
-            reactey = sum(
-                [
-                    get_molecule_energy_from_frame(
-                        frame=s_frame,
-                        # Modify name from xtb code.
-                        molecule_name=i.replace('_opt_xtb', ''),
-                        key='energy',
-                    )
-                    for i in intermediate['react']
-                ]
-            )
-
-            # Get reactant energies.
-            formey = prodey - reactey
-            formeyperimine = formey / intermediate['no.imine']
-            if formey > 0:
-                print(intermediate, solv, formey, formeyperimine)
-
-            # Add series to dataframe.
-            output_df.loc[count] = [
-                iname, lname, solv, ey, prodey, reactey, formey,
-                formeyperimine,
-            ]
-            count += 1
-
-    print(output_df)
-    output_df.to_csv(output_name, index=False)
 
 
 if __name__ == '__main__':
